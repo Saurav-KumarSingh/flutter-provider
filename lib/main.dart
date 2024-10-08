4829f83e9e70eb23c2106fdf715743a022282bc6
Saurav-KumarSingh <sauravkumarsingh093@gmail.com>
2024-10-08 07:47:33 +0530

    Counter using Provider done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_basic/counter_provider.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,6 +11,44 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ChangeNotifierProvider(
+        create: (_) => CounterProvider(),
+        child: HomePage(),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text('Home')),
+        body: Center(
+          child: Text(
+            '${context.watch<CounterProvider>().getCount()}',
+            style: const TextStyle(fontSize: 25),
+          ),
+        ),
+        bottomNavigationBar: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            ElevatedButton(
+                onPressed: () {
+                  context.read<CounterProvider>().incrementCount();
+                },
+                child: const Text('Add')),
+            ElevatedButton(
+                onPressed: () {
+                  context.read<CounterProvider>().decrementCount();
+                },
+                child: const Text('Sub')),
+          ],
+        ));
   }
 }
